d66e1328c4d76dc76aa861064ceff2742af04072
Kent Nolan <kent.nolan@gmail.com>
2026-01-08 15:46:56 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,13 +24,14 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-
     ingredients_string = ''
 
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    #st.write(ingredients_string
+
 
     my_insert_stmt = (
         "insert into smoothies.public.orders (ingredients, name_on_order) values ('"
@@ -48,10 +49,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your smoothie is ordered!')
     
-    #st.write(my_insert_stmt)
+
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json(), use_container_width=True)
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
